ec997e13697176e4b6eaae80bb392c734d9a6444
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-15 23:58:57 +0530

    Created structure of the backend user-api module

---
+++ user-api/src/main/java/com/eternity/userapi/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eternity.userapi.dao;
+
+import com.eternity.userapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ user-api/src/main/java/com/eternity/userapi/model/User.java
@@ -0,0 +1,48 @@
+package com.eternity.userapi.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
