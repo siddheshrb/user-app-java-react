cc85600306785c40b391ea4d77cb6d37ed11c3c3
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-16 00:21:07 +0530

    WIP: implementing backend APIs

--- user-api/src/main/java/com/eternity/userapi/controller/UserController.java
+++ user-api/src/main/java/com/eternity/userapi/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.eternity.userapi.controller;
 
+import com.eternity.userapi.model.User;
 import com.eternity.userapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(path = "/api")
 public class UserController {
 
     private final UserService userService;
@@ -14,6 +16,31 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping(path = "/")
+    private List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "/{id}")
+    private User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping(path = "/")
+    private User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping(path = "/")
+    private User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    private Long deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
     }
 
 }
--- user-api/src/main/java/com/eternity/userapi/service/UserService.java
+++ user-api/src/main/java/com/eternity/userapi/service/UserService.java
@@ -1,8 +1,11 @@
 package com.eternity.userapi.service;
 
 import com.eternity.userapi.dao.UserRepository;
+import com.eternity.userapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,4 +17,28 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        User u = userRepository.findById(user.getId()).orElse(null);
+        u.setName(user.getName());
+        u.setEmail(user.getEmail());
+        u.setPassword(user.getPassword());
+        return userRepository.save(u);
+    }
+
+    public Long deleteUser(Long id) {
+        userRepository.deleteById(id);
+        return id;
+    }
 }
