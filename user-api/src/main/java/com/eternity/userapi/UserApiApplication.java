37d37eeb0b6e0bc4be0b2c97d5c16895c3366d47
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-17 19:53:27 +0530

    Added swagger and mysql configurations

--- user-api/src/main/java/com/eternity/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/eternity/userapi/UserApiApplication.java
@@ -2,8 +2,10 @@ package com.eternity.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class UserApiApplication {
 
 	public static void main(String[] args) {
