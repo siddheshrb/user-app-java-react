b949e30f507c990ffbe449d396301ad1eb6e74ba
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-23 20:35:36 +0530

    Created navbar, form and table to list users

--- user-frontend/src/App.js
+++ user-frontend/src/App.js
@@ -1,12 +1,59 @@
 import React from 'react';
 //import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      Hello
+    <div className="container">
+      <div className="row">
+        <div className="col s6">
+          <form>
+            <div className="input-field col s12">
+              <i class="material-icons prefix">person</i>
+              <input type="text" id="autocomplete-input" class="autocomplete"/>
+              <label for="autocomplete-input">Autocomplete</label>
+            </div>
+            <div className="input-field col s12">
+              <i class="material-icons prefix">email</i>
+              <input type="email" id="autocomplete-input" class="autocomplete"/>
+              <label for="autocomplete-input">Email</label>
+            </div>
+            <div className="input-field col s12">
+              <i class="material-icons prefix">vpn_key</i>
+              <input type="password" id="autocomplete-input" class="autocomplete"/>
+              <label for="autocomplete-input">Password</label>
+            </div>
+            <button class="btn waves-effect waves-light right" type="submit" name="action">Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </form>
+        </div>
+        <div className="col s6">
+          <table>
+            <thread>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            <tbody>
+              <tr>
+                <td>Dummy</td>
+                <td>abc</td>
+                <td>pass</td>
+                <td>dummy</td>
+                <td>dummy</td>
+              </tr>
+            </tbody>
+            </thread>
+          </table>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
